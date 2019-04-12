6d1c3e7b6db5020371044d976e5e83ac02373902
mohamd7777 <48836916+mohamd7777@users.noreply.github.com>
2019-04-12 17:31:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,7 @@ const prefix = "m";
 
 
 
- {
-   console.log(`----------------`);
-      console.log(`script by mohamd`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : mohamd' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`=mbc all server by mohamd gamer`,"moahmd gamer")
-client.user.setStatus("dnd")
-});
+ 
 
 
 
